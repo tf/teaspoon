0fab57505d444a8d8503d4857eacc6bb0249720f
Tim Fischbach <tfischbach@codevise.de>
2018-02-22 10:27:48 +0100

    wip

--- lib/teaspoon/driver/selenium.rb
+++ lib/teaspoon/driver/selenium.rb
@@ -23,7 +23,8 @@ module Teaspoon
       end
 
       def run_specs(runner, url)
-        driver = ::Selenium::WebDriver.for(driver_options[:client_driver], @options[:client_driver_opts]&.dup || {})
+        driver = ::Selenium::WebDriver.for(driver_options[:client_driver],
+                                           @options[:client_driver_opts] || {})
         driver.navigate.to(url)
 
         ::Selenium::WebDriver::Wait.new(driver_options).until do
